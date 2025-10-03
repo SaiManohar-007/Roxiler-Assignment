e5ff42498b4be9aa62c91e393ff90af3ccbe3cb6
manohar <saimanohar.velaga007@gmail.com>
2025-10-03 11:43:53 +0530

    changed baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://roxiler-assignment-dr1u.onrender.com',
 });
 
 // Automatically attach token if it exists
